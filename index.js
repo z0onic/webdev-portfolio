7cf2d889b781aa224212888541260bfd6cc6c011
z0onic <59104479+z0onic@users.noreply.github.com>
2022-07-26 19:49:23 -0400

    first ship

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
+const title = document.querySelectorAll('.portfolio__item_title')
+const thumb = document.querySelectorAll('.portfolio__item')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
@@ -11,3 +13,14 @@ navLinks.forEach(link => {
     })
 })
 
+// thumb.addEventListener('mouseenter', () => {
+//     setTimeout(() => {
+//         title.style.opacity = '1'
+//     }, 600)
+// })
+
+// thumb.addEventListener('mouseleave', () => {
+//     setTimeout(() => {
+//         title.style.opacity = '0'
+//     }, 1000)
+// })
